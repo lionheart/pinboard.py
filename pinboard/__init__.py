cc312cad192487fd03734adad64669fe7115664c
Dan Loewenherz <dloewenherz@gmail.com>
2014-07-24 14:06:29 -0500

    update __all__

---
+++ pinboard/__init__.py
@@ -0,0 +1,21 @@
+from .pinboard import Pinboard
+
+from .metadata import (
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __maintainer__,
+    __version__,
+)
+
+__all__ = [
+    '__author__',
+    '__copyright__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+    'Pinboard',
+]
+
