7929b5181b02686489f8f2938d81f62b178b312e
Dan Loewenherz <dloewenherz@gmail.com>
2015-03-13 17:08:38 -0500

    update metdata

--- pinboard/metadata.py
+++ pinboard/metadata.py
@@ -1,6 +1,6 @@
-__version__ = "0.5.7"
+__version__ = "0.5.8"
 __author__ = "Dan Loewenherz"
-__copyright__ = "Copyright 2014, Lionheart Software"
+__copyright__ = "Copyright 2015, Lionheart Software"
 __maintainer__ = "Dan Loewenherz"
 __email__ = "dan@lionheartsw.com"
 __license__ = "Apache 2.0"
